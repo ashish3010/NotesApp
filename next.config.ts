1e5d8b9a4068bbc096c2ef62695e7c7faf25b060
Ashish Mishra <ashishmishra@Ashishs-Mac-mini.local>
2025-05-31 18:52:37 +0530

    gh pages added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const isGithubPages = process.env.NODE_ENV === "production";
+const repoName = "NotesApp";
 
+const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubPages ? `/${repoName}` : undefined,
+  assetPrefix: isGithubPages ? `/${repoName}/` : undefined,
+};
 export default nextConfig;
